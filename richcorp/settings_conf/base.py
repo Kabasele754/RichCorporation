c1459748af55d3a44a1ac2a772e70290f30a4314
Achille kabasele <pepexykabasele@gmail.com>
2026-02-10 19:35:29 +0200

    add good architecture app

--- richcorp/settings_conf/base.py
+++ richcorp/settings_conf/base.py
@@ -166,7 +166,7 @@ REST_FRAMEWORK = {
         "rest_framework.parsers.FormParser",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 3,
+    "PAGE_SIZE": 20,
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",

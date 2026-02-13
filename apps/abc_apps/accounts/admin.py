a2a8b546370a0475e7c6fdd9212db4b493d3de51
Achille kabasele <pepexykabasele@gmail.com>
2026-02-13 18:29:36 +0200

    update admin field

--- apps/abc_apps/accounts/admin.py
+++ apps/abc_apps/accounts/admin.py
@@ -12,7 +12,7 @@ class UserAdmin(DjangoUserAdmin):
     fieldsets = DjangoUserAdmin.fieldsets + (
         ("ABC Role", {"fields": ("role",)}),
     )
-    list_display = ("username", "email", "first_name", "last_name", "role", "is_staff", "is_active")
+    list_display = ("username", "email", "first_name", "last_name", "role", "is_staff", "is_active", "lat", "lng", "location_updated_at", "profile_photo"), 
     list_filter = ("role", "is_staff", "is_active")
     search_fields = ("username", "email", "first_name", "last_name")
 

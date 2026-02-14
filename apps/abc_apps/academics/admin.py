b96ca06539d3a0c5fe3db4ca03f489773bd5baa1
Achille kabasele <pepexykabasele@gmail.com>
2026-02-14 14:30:41 +0200

    update accedemin admin

--- apps/abc_apps/academics/admin.py
+++ apps/abc_apps/academics/admin.py
@@ -2,9 +2,10 @@
 # apps/academics/admin.py
 # =========================
 from django.contrib import admin
-from apps.abc_apps.academics.models import ClassRoom, Course, TeacherCourseAssignment, MonthlyGoal
+from apps.abc_apps.academics.models import ClassRoom, Course, StudentMonthlyEnrollment, TeacherCourseAssignment, MonthlyGoal
 
 admin.site.register(ClassRoom)
 admin.site.register(Course)
 admin.site.register(TeacherCourseAssignment)
 admin.site.register(MonthlyGoal)
+admin.site.register(StudentMonthlyEnrollment)

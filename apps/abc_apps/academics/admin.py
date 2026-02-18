8e84091cf84891209cb046a9aa7e5ba92e2c6a7e
Achille kabasele <pepexykabasele@gmail.com>
2026-02-18 10:06:05 +0200

    update room and add compus for track student behevious 10

--- apps/abc_apps/academics/admin.py
+++ apps/abc_apps/academics/admin.py
@@ -2,7 +2,7 @@
 # apps/academics/admin.py
 # =========================
 from django.contrib import admin
-from apps.abc_apps.academics.models import ClassRoom, Course, MonthlyClassGroup, StudentMonthlyEnrollment, TeacherCourseAssignment, MonthlyGoal
+from apps.abc_apps.academics.models import ClassRoom, Course, MonthlyClassGroup, SchoolCampus, StudentMonthlyEnrollment, TeacherCourseAssignment, MonthlyGoal
 
 admin.site.register(ClassRoom)
 admin.site.register(Course)
@@ -10,3 +10,4 @@ admin.site.register(TeacherCourseAssignment)
 admin.site.register(MonthlyGoal)
 admin.site.register(StudentMonthlyEnrollment)
 admin.site.register(MonthlyClassGroup)
+admin.site.register(SchoolCampus)

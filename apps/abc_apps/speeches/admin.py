e861183ae853612bd0d385db973d92f0fdfa2f06
Achille kabasele <pepexykabasele@gmail.com>
2026-02-20 05:16:07 +0200

    update admin like

--- apps/abc_apps/speeches/admin.py
+++ apps/abc_apps/speeches/admin.py
@@ -2,10 +2,13 @@
 # apps/speeches/admin.py
 # =========================
 from django.contrib import admin
-from apps.abc_apps.speeches.models import Speech, SpeechApproval, SpeechAudio,  SpeechCoaching
+from apps.abc_apps.speeches.models import Speech, SpeechApproval, SpeechAudio,  SpeechCoaching, SpeechComment, SpeechLike
 
 admin.site.register(Speech)
 admin.site.register(SpeechCoaching)
 admin.site.register(SpeechAudio)
 admin.site.register(SpeechApproval)
+admin.site.register(SpeechLike)
+admin.site.register(SpeechComment)
+
 

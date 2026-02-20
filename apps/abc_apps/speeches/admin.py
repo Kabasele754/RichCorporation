7b5b7ace02cf0b4b7c86e0ebe8fa82b1954b3a22
Achille kabasele <pepexykabasele@gmail.com>
2026-02-20 03:20:40 +0200

    fix bug 500 admin

--- apps/abc_apps/speeches/admin.py
+++ apps/abc_apps/speeches/admin.py
@@ -2,9 +2,10 @@
 # apps/speeches/admin.py
 # =========================
 from django.contrib import admin
-from apps.abc_apps.speeches.models import Speech, SpeechAudio,  SpeechCoaching
+from apps.abc_apps.speeches.models import Speech, SpeechApproval, SpeechAudio,  SpeechCoaching
 
 admin.site.register(Speech)
 admin.site.register(SpeechCoaching)
 admin.site.register(SpeechAudio)
+admin.site.register(SpeechApproval)
 

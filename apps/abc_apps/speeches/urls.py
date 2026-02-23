b135c79e04294cf9546b881c6c9fd2736e32614f
Achille kabasele <pepexykabasele@gmail.com>
2026-02-23 07:07:55 +0200

    update action public ee

--- apps/abc_apps/speeches/urls.py
+++ apps/abc_apps/speeches/urls.py
@@ -7,6 +7,15 @@ from apps.abc_apps.speeches.views import SpeechViewSet
 
 router = DefaultRouter()
 router.register(r"speeches", SpeechViewSet, basename="speeches")
+speech_like = SpeechViewSet.as_view({"post": "like"})
+speech_comment = SpeechViewSet.as_view({"post": "comment"})
+speech_comments = SpeechViewSet.as_view({"get": "comments"})
 # router.register(r"actions", SpeechActionsViewSet, basename="speech-actions")
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+        path("", include(router.urls)),
+    path("speeches/like/", speech_like, name="speech-like"),
+    path("speeches/comment/", speech_comment, name="speech-comment"),
+    path("speeches/comments/", speech_comments, name="speech-comments"),
+    
+]

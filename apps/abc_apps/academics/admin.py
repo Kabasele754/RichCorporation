ae4b58e609bc5ea501e5fe867ebedb42a653bd2a
Achille kabasele <pepexykabasele@gmail.com>
2026-03-05 17:14:03 +0200

    update

--- apps/abc_apps/academics/admin.py
+++ apps/abc_apps/academics/admin.py
@@ -2,7 +2,7 @@
 # apps/academics/admin.py
 # =========================
 from django.contrib import admin
-from apps.abc_apps.academics.models import ClassRoom, Course, MonthlyClassGroup, SchoolCampus, StudentMonthlyEnrollment, TeacherCourseAssignment, MonthlyGoal
+from apps.abc_apps.academics.models import ClassRoom, Course, MonthlyClassGroup, Room, SchoolCampus, StudentMonthlyEnrollment, TeacherCourseAssignment, MonthlyGoal
 
 admin.site.register(ClassRoom)
 admin.site.register(Course)
@@ -11,3 +11,4 @@ admin.site.register(MonthlyGoal)
 admin.site.register(StudentMonthlyEnrollment)
 admin.site.register(MonthlyClassGroup)
 admin.site.register(SchoolCampus)
+admin.site.register(Room)
